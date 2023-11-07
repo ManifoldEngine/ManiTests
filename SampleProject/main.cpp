3e3c17bd3702099f9dcf81105d44d33002511e41
Nicolas Hamard <nox.tux@gmail.com>
2023-11-07 02:05:16 -0500

    Update main.cpp
    Fixed line break issue

--- SampleProject/main.cpp
+++ SampleProject/main.cpp
@@ -1,11 +1,13 @@
 #include "simpleTests.h"
 #include <iostream>
 
-ST_TEST(Test1, "should fail") {
+ST_TEST(Test1, "should fail") 
+{
 	ST_ASSERT(false, "assert false");
 }
 
-ST_TEST(Test2, "should pass") {
+ST_TEST(Test2, "should pass") 
+{
 	ST_ASSERT(true, "assert true");
 }
 
